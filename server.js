ba65d57d83a2c92f0efe1077048370ef8f7386ca
Youstina Ibrahim <youstinaaibrahim@gmail.com>
2020-08-06 21:33:45 -0400

    EatDaBurger

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(express.static("public"))
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({
     extended: true
