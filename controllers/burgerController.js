77104f1df1539e2c6604559d78b2d5cc57ba2728
Youstina Ibrahim <youstinaaibrahim@gmail.com>
2020-08-07 12:24:35 -0400

    EatDFaBurger

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -16,8 +16,7 @@ router.get("/burgers", function (req, res) {
 });
 
 router.post("/burgers/create", function (req, res) {
-    burger.create( req.body.burger_name
-    , function (result) {
+    burger.create( req.body.burger_name, function (result) {
         // Send back the ID of the new quote
         res.redirect("/");
     });
